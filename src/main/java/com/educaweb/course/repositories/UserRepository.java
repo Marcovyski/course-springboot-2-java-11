ff9ee3af526f0904d65c8ef2ef82c8718c34f10e
Marcovyski <marco_a_costa@hotmail.com>
2022-02-02 19:21:01 -0300

    Order,Instant, ISO 8601 - PART 2

--- src/main/java/com/educaweb/course/repositories/UserRepository.java
+++ src/main/java/com/educaweb/course/repositories/UserRepository.java
@@ -2,8 +2,8 @@ package com.educaweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.educaweb.course.entities.User;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
