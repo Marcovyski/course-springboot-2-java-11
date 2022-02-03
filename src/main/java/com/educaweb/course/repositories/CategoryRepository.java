5e07b80c7ff1bc0883ce57a20c75355359a0ae43
Marcovyski <marco_a_costa@hotmail.com>
2022-02-03 18:47:50 -0300

    Category

---
+++ src/main/java/com/educaweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.educaweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educaweb.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
