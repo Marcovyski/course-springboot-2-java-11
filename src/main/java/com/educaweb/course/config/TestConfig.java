8cec44ca0911ab5edca3620afdcb1f77956c984e
Marcovyski <marco_a_costa@hotmail.com>
2022-02-11 17:37:18 -0300

    OrderItem, many-to-many association with extra atributes PART 2

--- src/main/java/com/educaweb/course/config/TestConfig.java
+++ src/main/java/com/educaweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educaweb.course.entities.Category;
 import com.educaweb.course.entities.Order;
+import com.educaweb.course.entities.OrderItem;
 import com.educaweb.course.entities.Product;
 import com.educaweb.course.entities.User;
 import com.educaweb.course.entities.enums.OrderStatus;
 import com.educaweb.course.repositories.CategoryRepository;
+import com.educaweb.course.repositories.OrderItemRepository;
 import com.educaweb.course.repositories.OrderRepository;
 import com.educaweb.course.repositories.ProductRepository;
 import com.educaweb.course.repositories.UserRepository;
@@ -30,7 +32,8 @@ public class TestConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
-
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -56,11 +59,6 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
-		
-		
-		
-		
-
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -70,6 +68,17 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		
+		
 	}
 
 }
